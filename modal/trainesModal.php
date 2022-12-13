ef1945ef850b985f99e45b3c714ad151121255c1
abdelaziz <abdelaazizeelhathoute.2018@gmail.com>
2022-12-13 11:56:53 +0100

    search by ajax

--- modal/trainesModal.php
+++ modal/trainesModal.php
@@ -28,5 +28,16 @@ require_once '../classes/traines.class.php';
                 $stm = $this->connexion()->prepare($sql);
                  $stm->execute([$id]);
     }
+
+    public function searchTrain($nom){
+        $sql = "SELECT * FROM gares WHERE nom like ? ORDER BY nom";
+        $stm = $this->connexion()->prepare($sql);
+        $stm->execute([$nom]);
+        $result = $stm->fetchAll();
+        return $result;
+    }
+
+    
+          
           
 }
